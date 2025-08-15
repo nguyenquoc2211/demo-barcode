0cebcd0a51e3b47a3d1768f1e2bf0294e93c7eef
Wood <quocnp1@hasaki.vn>
2025-08-15 23:59:50 +0700

    init repo

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import InputBarcode from "docker-manager-web/components/input-barcode/InputBarcode";
+
+
+export default function Home() {
+  return (
+    <div>
+        <h1>Demo barcode</h1>
+        <InputBarcode />
+    </div>
+  );
+}
---
+++ src/components/input-barcode/InputBarcode.tsx
@@ -0,0 +1,36 @@
+"use client"
+import {useEffect, useRef, useState} from "react";
+
+function InputBarcode() {
+  const bufferRef = useRef<string>('');
+  const [scanned, setScanned] = useState<string>('');
+
+  const handleInputBarcode = (e: KeyboardEvent)=> {
+    if (e.key === 'Enter' || e.key === 'Return') {
+      setScanned(bufferRef.current);
+      bufferRef.current = '';
+    }
+      else {
+      bufferRef.current += e.key;
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleInputBarcode)
+
+    // Clear when hook is destroyed
+    return () => document.removeEventListener('keydown', handleInputBarcode);
+  }, []);
+
+  useEffect(() => {
+    console.log("Scanned", scanned)
+  }, [scanned]);
+  return (
+    <div>
+      <h2>🔍 Quét mã QR / Barcode (Không cần focus)</h2>
+      <p>📦 Dữ liệu đã quét: <strong>{scanned}</strong></p>
+    </div>
+  )
+}
+
+export default InputBarcode;
